f95e734fb48855ec20dc8bf4ceb97c97c15c11fe
sam-flores <111543031+epdx@users.noreply.github.com>
2022-08-31 15:31:39 -0700

    fsd

---
+++ show.js
@@ -0,0 +1,14 @@
+const myHeaders = new Headers();
+
+const myRequest = new Request('GET /api/v1/sites/f98d50f5-6ad4-4119-a051-374f8094c048/submissions', {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'cors',
+  cache: 'default',
+});
+
+fetch(myRequest)
+  .then((response) => response.blob())
+  .then((myBlob) => {
+    myImage.src = URL.createObjectURL(myBlob);
+  });
